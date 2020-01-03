99a0ba00f90f26cafc6ba6e5a3c40e3b048870ae
Sadvlad <Sadvlads@tut.by>
2020-01-03 16:57:28 +0300

    step limit
    add step limit for word guessing

--- script.js
+++ script.js
@@ -11,7 +11,8 @@ for (var i = 0;i < word.length; i++){
     answerarray[i] = "_";
 }
 var remainingLetters = word.length;
-while (remainingLetters > 0){
+var stepLimit = 12;
+while (remainingLetters > 0 && stepLimit > 0){
 
     alert(answerarray.join(" "));
 var guess = prompt("Угадайте букву или нажмите отмена");
@@ -20,6 +21,7 @@ if (guess === null){
 } else if (guess.length !== 1){
     alert("Пожалуйста введите только одну букву.");
 } else {
+    stepLimit--;
     guess = guess.toLowerCase();
 
     for (var j = 0; j < word.length; j++) {
@@ -32,4 +34,9 @@ if (guess === null){
 }
 }
 alert(answerarray.join(" "));
+if (stepLimit > 0) {
 alert("Отлично! Было загадано слово " + word);
+}
+else {
+alert("Отправляйтесь на висилицу! Было загадано слово " + word);    
+}
